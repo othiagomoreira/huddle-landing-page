bb8751c663ed20f48d65f80941289459274b6a90
Thiago Moreira <contatothiagomoreira@outlook.com>
2021-12-03 00:28:18 -0300

    .

--- app/js/script.js
+++
@@ -1,5 +0,0 @@
-console.log('HELLO');
-
-const test = () => {
-	console.log('this is a test');
-};
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";console.log("HELLO");var test=function(){console.log("this is a test")};
+"use strict";
 //# sourceMappingURL=script.js.map
